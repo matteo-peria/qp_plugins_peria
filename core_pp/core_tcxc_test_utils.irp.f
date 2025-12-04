3d2c9a5cc58ba5e42fa8a5dd5fa0c9728c57629f
matteo-peria <math.peria@gmail.com>
2025-12-04 14:29:03 +0100

    Included a way of computing exact integral for test

--- core_pp/core_tcxc_test_utils.irp.f
+++ core_pp/core_tcxc_test_utils.irp.f
@@ -24,12 +24,53 @@ BEGIN_PROVIDER [ double precision, ao_overlap_grid1, (ao_num, ao_num)]
       end do
     end do
   end do
-
-  !! linear algebra faster solution:
-  !! Hadamard product of aos_in_r_array*aos_in_r_array 
-  !! and matrix product with grid weights array
-  !call get_AB_prod( aos_in_r_array*aos_in_r_array &
-  !                , ao_num, n_points_final_grid   &
-  !                , final_weight_at_r_vector, 1   &
-  !                , ao_overlap_grid1)
 END_PROVIDER
+
+
+BEGIN_PROVIDER [ double precision, core_density_matrix, (ao_num, ao_num)]
+  BEGIN_DOC
+  ! Compute the density matrix of the core orbitals
+  END_DOC
+  implicit none
+  integer :: nu, mu, mo_c
+  double precision :: c_mu, c_nu
+
+  core_density_matrix(:,:) = 0.d0
+  do nu = 1, ao_num
+    do mu = 1, ao_num
+      do mo_c = 1, n_core_pseudo_orb
+        c_mu = mo_coef(mu, list_core_pseudo(mo_c))
+        c_nu = mo_coef(nu, list_core_pseudo(mo_c))
+        core_density_matrix(mu,nu) += c_mu * c_nu 
+      end do
+    end do
+  end do
+END_PROVIDER
+
+
+BEGIN_PROVIDER [ double precision, core_xpot_exact_ao, (ao_num, ao_num)]
+  implicit none
+  BEGIN_DOC
+  ! Numerical evaluation of
+  ! < l | V_x^{core_pseudo} | j >
+  ! = -\sum_{m\in\text{core}} < l | \phi_m(1)\phi_m(2)/r12 | k >
+  ! = \int dr_1 \int dr2 \chi_l(r_1) V_x(r_1,r_2) \chi_j(r_2)
+  ! = - \int dr_1 \int dr_2 \chi_l(r_1) \sum_{m\in\text{core}} (\phi_m(r_1)\phi_j(r_2)/|r-r_2|) \chi_j(r_2)
+  END_DOC
+  integer :: j, l, nu, mu
+  core_xpot_exact_ao(:,:) = 0.d0
+
+  do j = 1, ao_num
+    do l = 1, ao_num
+      do nu = 1, n_core_pseudo_orb
+        do mu = 1, n_core_pseudo_orb
+          core_xpot_exact_ao(l,j) -= ao_two_e_integral(l,nu,mu,j) * core_density_matrix(mu,nu)
+        enddo
+      enddo
+    enddo
+  enddo
+
+END_PROVIDER
+
+
+
--- core_pp/core_xpot_test.irp.f
+++ core_pp/core_xpot_test.irp.f
@@ -24,15 +24,6 @@ program core_xpot_test
     call test_grids_core_xpot()
   end if
 
-
-  !if (command_argument_count() < 1) then
-  !  print *, "Usage: ./append_line_main <filename>"
-  !  stop 1
-  !end if
-
-  !call get_command_argument(1, filename)
-
-
 end program
 
 
@@ -49,6 +40,7 @@ subroutine test_grids_core_xpot(filename)
   double precision :: diff_prun1_full3
   double precision :: diff_prun1_full3_old
   double precision :: diff_prun1_prun3
+  double precision :: diff_prun1_fuzzy
 
   integer :: ios
   integer :: unit_out
@@ -102,166 +94,15 @@ subroutine test_grids_core_xpot(filename)
 
   write(*,'(A)') repeat('=', 70)
 
+  print*,'CORE_XPOT_FUZZY_GRID'
+  call compute_dp_array_diff(core_xpot_fuzzy_grid, &
+                         & core_xpot_exact, &
+                         & show = .False.,              &
+                         & row_indx=list_valence_pseudo,       &
+                         & col_indx=list_valence_pseudo, &
+                         & diff=diff_prun1_fuzzy)
 
-!!!!  logical :: file_exists
-!!!!  if (present(filename)) then
-!!!!    output = trim(filename)//'/core_xpot.out'
-!!!!    inquire(file=output, exist=file_exists)
-!!!!    
-!!!!    if (file_exists) then
-!!!!      open(newunit=unit_out, & 
-!!!!         & file=output,    & 
-!!!!         & status='old',     & 
-!!!!         & position='append',&  
-!!!!         & action='write',   & 
-!!!!         & iostat=ios        & 
-!!!!         &)
-!!!!    else
-!!!!      open(newunit=unit_out, & 
-!!!!         & file=output,    & 
-!!!!         & status='new',     & 
-!!!!         & action='write',   & 
-!!!!         & iostat=ios        & 
-!!!!         &)
-!!!!    end if
-!!!!
-!!!!    if (ios /= 0) then
-!!!!      print *, "Error opening file: ", trim(filename)
-!!!!      stop 1
-!!!!    end if
-!!!!
-!!!!  else
-!!!!    unit_out=output_unit
-!!!!  end if
-!!!!
-!!!!  character(len=100) :: fmt_str
-!!!!  fmt_str = '(14A8, 6A12)'
-!!!!  if (.not.file_exists) then
-!!!!    write(unit_out, fmt_str) &
-!!!!                       & 'N_rad1', 'N_ang1', 'N_sph1', 'N_ful1', 'N_pru1', &
-!!!!                       & 'N_rad2', 'N_ang2', 'N_sph2', 'N_ful2', 'N_pru2', &
-!!!!                       & 'N_rad3', 'N_ang3', 'N_sph3', 'N_ful3',           &
-!!!!                       & 'TOT(p1+f2)', &
-!!!!                       & 'RES(p1+f2)', &
-!!!!                       & 'TOT(p1+p2)', &
-!!!!                       & 'RES(p1+p2)', &
-!!!!                       & 'TOT(p1+f3)', &
-!!!!                       & 'RES(p1+f3)'
-!!!!  end if
-!!!!
-!!!!  character(len=100) :: fmt_num
-!!!!  fmt_num = '(14I8, I12, E12.3, I12, E12.3, I12, E12.3)'
-!!!!
-!!!!  write(unit_out, fmt_num)                                &
-!!!!      & n_points_rad_grid,                            &
-!!!!      & n_points_ang_grid,                            &
-!!!!      & n_points_rad_grid*n_points_ang_grid,          &
-!!!!      & nucl_num*n_points_rad_grid*n_points_ang_grid, &
-!!!!      & n_points_final_grid,                          &
-!!!!      !
-!!!!      & n_points_rad_extra_grid,                                  &
-!!!!      & n_points_ang_extra_grid,                                  &
-!!!!      & n_points_rad_extra_grid*n_points_ang_extra_grid,          &
-!!!!      & nucl_num*n_points_rad_extra_grid*n_points_ang_extra_grid, &
-!!!!      & n_points_extra_final_grid,                                &
-!!!!      !
-!!!!      & n_points_rad_adapt_grid,                                  &                 
-!!!!      & n_points_ang_adapt_grid,                                  &
-!!!!      & n_points_rad_adapt_grid*n_points_ang_adapt_grid,          &
-!!!!      & nucl_num*n_points_rad_adapt_grid*n_points_ang_adapt_grid, &
-!!!!      !
-!!!!      & nucl_num*(n_points_final_grid                               &
-!!!!             &  + n_points_rad_extra_grid*n_points_ang_extra_grid), &
-!!!!      & diff_prun1_full2,                                           &
-!!!!      !
-!!!!      & nucl_num*(n_points_final_grid + n_points_extra_final_grid), &
-!!!!      & diff_prun1_prun2,                                           &
-!!!!      !
-!!!!      & nucl_num*(n_points_final_grid*(                          &
-!!!!          & 1 + n_points_rad_adapt_grid*n_points_ang_adapt_grid) &
-!!!!          & + n_points_rad_extra_grid*n_points_ang_extra_grid),  &
-!!!!      & diff_prun1_full3
-!!!!
-!!!!  if (present(filename).and.ios==0) then
-!!!!    close(unit_out)
-!!!!  end if
-!!!!
-!!!!!
-!!!!!
-!!!!!  write(*,'(A14, A14, A14, A14)') &
-!!!!!      &'Type', 'Grid 1', 'Grid 2', 'Grid 3'
-!!!!!
-!!!!!  write(*,'(A14, I14, I14, I14)') &
-!!!!!      & 'Radial',                 &
-!!!!!      & n_points_rad_grid,        &
-!!!!!      & n_points_rad_extra_grid,  &
-!!!!!      & n_points_rad_adapt_grid
-!!!!!
-!!!!!  write(*,'(A14, I14, I14, I14)') &
-!!!!!      & 'Angular',                &
-!!!!!      & n_points_ang_grid,        &
-!!!!!      & n_points_ang_extra_grid,  &
-!!!!!      & n_points_ang_adapt_grid
-!!!!!
-!!!!!  write(*,'(A14, I14, I14, I14)')                        &
-!!!!!      & 'Tot sphe',                                      & 
-!!!!!      & n_points_rad_grid*n_points_ang_grid,             &
-!!!!!      & n_points_rad_extra_grid*n_points_ang_extra_grid, &
-!!!!!      & n_points_rad_adapt_grid*n_points_ang_adapt_grid
-!!!!!
-!!!!!  write(*,'(A14, I14, I14, I14)')                                   &
-!!!!!      & 'Tot molec',                                                & 
-!!!!!      & nucl_num*n_points_rad_grid*n_points_ang_grid,                 &
-!!!!!      & nucl_num*n_points_rad_extra_grid*n_points_ang_extra_grid,     &
-!!!!!      & nucl_num*n_points_rad_grid*n_points_ang_grid*(1               &
-!!!!!          + nucl_num*n_points_rad_adapt_grid*n_points_ang_adapt_grid) &
-!!!!!          + nucl_num*n_points_rad_extra_grid*n_points_ang_extra_grid
-!!!!!
-!!!!!  write(*,'(A14, I14, I14, A14)')  &
-!!!!!      & 'Tot pruned',              &
-!!!!!      & n_points_final_grid,       &
-!!!!!      & n_points_extra_final_grid, &
-!!!!!      & 'no pruning'
-!!!!!
-!!!!!
-!!!!!  write(*,'(A)') repeat('=', 70)
-!!!!!
-!!!!!  write(*,'(A20, A20, A20, A20, A20)') &
-!!!!!      & 'Grid combo',             &
-!!!!!      & 'Pruned_1 + Full_2',    &
-!!!!!      & 'Pruned_1 + Pruned_2',    &
-!!!!!      & 'Pruned_1 + Full_3',    &
-!!!!!      & 'Pruned_1 + Pruned_3'
-!!!!!  
-!!!!!  write(*,'(A20, I20, I20, I20, I20)')                                &
-!!!!!      & 'N of points',                                                &
-!!!!!      & n_points_final_grid                  &
-!!!!!          + nucl_num*n_points_rad_extra_grid*n_points_ang_extra_grid, &
-!!!!!      & n_points_final_grid                                           &
-!!!!!          + n_points_extra_final_grid, &
-!!!!!      & n_points_final_grid*(1                                      &
-!!!!!      &   + nucl_num*n_points_rad_adapt_grid*n_points_ang_adapt_grid) &
-!!!!!          + nucl_num*n_points_rad_extra_grid*n_points_ang_extra_grid, &
-!!!!!      & -9999999
-!!!!!
-!!!!!  write(*,'(A20, E20.10, E20.10, E20.10, E20.10)') & 
-!!!!!      & 'Diff with exact',             &
-!!!!!      & diff_prun1_full2,              &
-!!!!!      & diff_prun1_prun2,              &
-!!!!!      & diff_prun1_full3,              &
-!!!!!      & diff_prun1_prun3
-!!!!!
-!!!!
-!!!!!!!!!  write(*,*) 'grid_points_radial', grid_points_radial
-!!!!!!!!!
-!!!!!!!!!  integer :: j
-!!!!!!!!!  double precision :: x, r
-!!!!!!!!!  double precision, external :: knowles_function
-!!!!!!!!!  do j = 1, n_points_radial_grid-1                                                                
-!!!!!!!!!    x = grid_points_radial(j)                                                                     
-!!!!!!!!!    r = knowles_function(alpha_knowles(grid_atomic_number(1)), m_knowles, x)                      
-!!!!!!!!!    write(*,*) j, x, r
-!!!!!!!!!  end do
-!!!!
-!!!!
+  write(*,'(A)') repeat('=', 70)
+
+
 end subroutine
